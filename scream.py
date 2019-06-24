4646bcfdebe1d6cf85aa23882fdf99e23d2f7237
Michal Fasanek <michal.fasanek@naytrolabs.com>
2019-06-25 01:17:36 +0200

    Added functioning main script that loads, sanitizes and ads defaults to cmd kwargs

---
+++ scream.py
@@ -0,0 +1,54 @@
+import sys
+
+def sanitize_input(**kwargs) -> dict:
+
+    allowed_kwargs = [
+        '--apikey',
+        '--mode'
+    ]
+
+    sanitized_kwargs = {}
+
+    for key, value in kwargs.items():
+        
+        if key not in allowed_kwargs:
+            print(f'Unknown input attribute: {key}')
+            print('Aborting execution')
+            exit(1)
+
+        else:
+            sanitized_kwargs[key.lstrip('-')] = value
+
+    return sanitized_kwargs
+
+def apply_defaults(**kwargs) -> dict:
+
+    if 'mode' not in kwargs.keys():
+        kwargs['mode'] = 'office'
+    
+    if 'apikey' not in kwargs.keys():
+        
+        try:
+            with open('apikey') as api_key_file:
+                kwargs['apikey'] = api_key_file.read()
+        
+        except IOError:
+            print('API-key not provided on-start')
+            print('API-key file not available[missing/permissions/corrupted]')
+            print('Aborting execution')
+            exit(1)
+
+    return kwargs
+
+
+def main(**kwargs):
+
+    kwargs = sanitize_input(**kwargs)
+    kwargs = apply_defaults(**kwargs)
+    
+    print('Runtime settings:')
+    [print(f'\t{key}={value}') for key, value in kwargs.items()]
+
+if __name__ == '__main__':
+
+    main(**dict(arg.split('=') for arg in sys.argv[1:]))
